ac1aa6b7b6ebbb2a3847ac953e74a53f8848f4a0
jfethers <jillfetherston@gmail.com>
2023-05-23 12:51:35 -0400

    disabling sendgrid

--- server/index.js
+++ server/index.js
@@ -4,41 +4,41 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require("path");
 // sendgrid
-const nodemailer = require("nodemailer");
-const sendGridTransport = require("nodemailer-sendgrid-transport");
-const { SENDGRID_API } = require("../config/keys");
+// const nodemailer = require("nodemailer");
+// const sendGridTransport = require("nodemailer-sendgrid-transport");
+// const { SENDGRID_API } = require("../config/keys");
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("client/build"));
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+	res.sendFile(path.resolve(__dirname, "/../../client/build/index.html"));
 });
 
-const transporter = nodemailer.createTransport(
-	sendGridTransport({
-		auth: {
-			api_key: SENDGRID_API,
-		},
-	})
-);
+// const transporter = nodemailer.createTransport(
+// 	sendGridTransport({
+// 		auth: {
+// 			api_key: SENDGRID_API,
+// 		},
+// 	})
+// );
 
-app.post("/api/send", (req, res) => {
-	transporter
-		.sendMail({
-			to: "jillfetherston@gmail.com",
-			from: "jillfetherston@gmail.com",
-			subject: req.body.subject,
-			html: `<h3>${req.body.name}</h3>
-			<h3>from: ${req.body.email}</h3>
-			<p>${req.body.message}</p>
-			`,
-		})
-		.then((resp) => {
-			res.json({ resp });
-		});
-});
+// app.post("/api/send", (req, res) => {
+// 	transporter
+// 		.sendMail({
+// 			to: "jillfetherston@gmail.com",
+// 			from: "jillfetherston@gmail.com",
+// 			subject: req.body.subject,
+// 			html: `<h3>${req.body.name}</h3>
+// 			<h3>from: ${req.body.email}</h3>
+// 			<p>${req.body.message}</p>
+// 			`,
+// 		})
+// 		.then((resp) => {
+// 			res.json({ resp });
+// 		});
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
