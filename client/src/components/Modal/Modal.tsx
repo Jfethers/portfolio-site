a1e47f76517d0ac3fe7e598749316f8f6dc6ce08
jfethers <jillfetherston@gmail.com>
2023-05-24 11:26:51 -0400

    close modal on click outside

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,4 +1,10 @@
-import React, { FunctionComponent, ReactNode, Ref, RefObject } from "react";
+import React, {
+	FunctionComponent,
+	ReactNode,
+	useRef,
+	useEffect,
+	RefObject,
+} from "react";
 import classNames from "classnames";
 import { CgClose } from "react-icons/cg";
 
@@ -11,16 +17,21 @@ interface IModalProps {
 const Modal: FunctionComponent<IModalProps> = (props: IModalProps) => {
 	const { showModal, children, setShowModal } = props;
 
+	const ref = useRef<HTMLDivElement>(null);
+
+	useOnClickOutside(ref, () => {
+		setShowModal(false);
+	});
+
 	return (
 		<div
-			onClick={() => setShowModal(false)}
 			className={classNames({
 				"modal-overlay": true,
 				active: showModal,
 			})}
 		>
 			{showModal ? (
-				<div className='modal-body'>
+				<div className='modal-body' ref={ref}>
 					<CgClose className='close' onClick={() => setShowModal(false)} />
 					<div className='modal-contents'>{children}</div>
 				</div>
@@ -28,5 +39,25 @@ const Modal: FunctionComponent<IModalProps> = (props: IModalProps) => {
 		</div>
 	);
 };
+
+function useOnClickOutside(ref: RefObject<HTMLDivElement>, handler: Function) {
+	useEffect(() => {
+		const listener = (event: MouseEvent | TouchEvent) => {
+			if (
+				(ref && !ref?.current) ||
+				(ref?.current && ref?.current.contains(event.target as Node))
+			) {
+				return;
+			}
+			handler(event);
+		};
+		document.addEventListener("mousedown", listener);
+		document.addEventListener("touchstart", listener);
+		return () => {
+			document.removeEventListener("mousedown", listener);
+			document.removeEventListener("touchstart", listener);
+		};
+	}, [ref, handler]);
+}
 
 export default Modal;
